b7b94b3ce91e286e1882b357add6f8c48f91a4c7
Yingyang Zhang <yingyang.zhang95@gmail.com>
2022-09-19 13:27:18 -0400

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function focusE () {
+    console.log('Good!');
+}
+
+function blurE () {
+    console.log('Hey! Eyes on me!');
+}
+
+function EyesOneMe() {
+    return (
+        <div>
+            <button onFocus = {focusE} onBlur = {blurE}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOneMe;
